9d177bba8859e8af19e51b12eadab1c84ee50c1d
LucasTamayaGIT <lucastamaya.974@gmail.com>
2022-01-03 15:47:20 +0100

    Grid item: OK. Sauf petit bug au niveau du svg Self Care

--- src/components/MainGridContainer.js
+++ src/components/MainGridContainer.js
@@ -1,15 +1,15 @@
 const MainGridContainer = () => {
     return (
         <div>
-            <div class="main-grid-container">
-                <div class="reporter-container">
-                    <div class="avatar"></div> 
-                    <div class="reporter-txt">
+            <div className="main-grid-container">
+                <div className="reporter-container">
+                    <div className="avatar"></div> 
+                    <div className="reporter-txt">
                         <p>Report for</p>
                         <p>Jeremy Robson</p>
                     </div>
                 </div>
-                <div class="timeframe">
+                <div className="timeframe">
                     <a id="daily" href="#">Daily</a>
                     <a id="weekly" href="#">Weekly</a>
                     <a id="monthly" href="#">Monthly</a>
--- src/vue/DailyVue.js
+++ src/vue/DailyVue.js
@@ -1,107 +1,36 @@
 import { useState } from "react";
-import useFetch from "../useFetch";
+import { v4 as uuidv4 } from 'uuid';
 import MainGridContainer from "../components/MainGridContainer";
+const dataJson = require("../data/data.json") //Import de data.json
 
 const DailyVue = () => {
-
-    const { data, chargement } = useFetch("../../public/data/data.json");
 
     return (
 
         <main>
-            <div class="grid">
-
+            <div className="grid">
                 <MainGridContainer />
 
-                <div class="work">
-                    <img src="./images/icon-work.svg" alt="icon work" />
-                    <div class="item-container">
-                        <div class="current-time">
-                            <h2>Work</h2>
-                            <p>32hrs</p>
-                        </div>
-                        <div class="previous-time">
-                            <img src="./images/icon-ellipsis.svg" alt="icon ellipsis" />
-                            <p>Last week - 36hrs</p>
-                        </div>
-                    </div>
-                </div>
+                {dataJson.map(x => (
 
-                <div class="play">
-                    <img src="./images/icon-play.svg" alt="icon play" />
-                    <div class="item-container">
-                        <div class="current-time">
-                            <h2>Work</h2>
-                            <p>10hrs</p>
-                        </div>
-                        <div class="previous-time">
-                            <img src="./images/icon-ellipsis.svg" alt="icon ellipsis" />
-                            <p>Last week - 8hrs</p>
+                    <div className={`${x.title}`} key={uuidv4()}>
+                        <img src={`./images/icon-${x.title}.svg`} alt="icon" />
+                        <div className="item-container">
+                            <div className="current-time">
+                                <h2>{x.title}</h2>
+                                <p>{x.timeframes.daily.current}hrs</p>
+                            </div>
+                            <div className="previous-time">
+                                <img src="./images/icon-ellipsis.svg" alt="icon ellipsis" />
+                                <p>Last week - {x.timeframes.daily.previous}hrs</p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="study">
-                    <img src="./images/icon-study.svg" alt="icon study" />
-                    <div id="study" class="item-container">
-                        <div class="current-time">
-                            <h2>Work</h2>
-                            <p>4hrs</p>
-                        </div>
-                        <div class="previous-time">
-                            <img src="./images/icon-ellipsis.svg" alt="icon ellipsis" />
-                            <p>Last week - 7hrs</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="exercise">
-                    <img src="./images/icon-exercise.svg" alt="icon exercise" />
-                    <div id="exercise" class="item-container">
-                        <div class="current-time">
-                            <h2>Work</h2>
-                            <p>4hrs</p>
-                        </div>
-                        <div class="previous-time">
-                            <img src="./images/icon-ellipsis.svg" alt="icon ellipsis" />
-                            <p>Last week - 5hrs</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="social">
-                    <img src="./images/icon-social.svg" alt="icon social" />
-                    <div id="social" class="item-container">
-                        <div class="current-time">
-                            <h2>Work</h2>
-                            <p>5hrs</p>
-                        </div>
-                        <div class="previous-time">
-                            <img src="./images/icon-ellipsis.svg" alt="icon ellipsis" />
-                            <p>Last week - 10hrs</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="self-care">
-                    <img src="./images/icon-self-care.svg" alt="icon selfcare" />
-                    <div id="self-care" class="item-container">
-                        <div class="current-time">
-                            <h2>Work</h2>
-                            <p>2hrs</p>
-                        </div>
-                        <div class="previous-time">
-                            <img src="./images/icon-ellipsis.svg" alt="icon ellipsis" />
-                            <p>Last week - 2hrs</p>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
-
         </main>
-
     )
-
 }
 
 export default DailyVue;
