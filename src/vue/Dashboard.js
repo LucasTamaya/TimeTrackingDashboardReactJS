17a97b4a7a6adb4a2dc5dad291d2bca0a174af55
LucasTamayaGIT <lucastamaya.974@gmail.com>
2022-01-05 08:13:48 +0100

    placement et dimension des éléments pour la version laptop: OK

--- src/vue/Dashboard.js
+++ src/vue/Dashboard.js
@@ -10,26 +10,25 @@ const Dashboard = () => {
     return (
 
         <main>
-            <div className="grid">
-                <MainGridContainer setTimeFrame={setTimeFrame}/>
-
-                {timeFrame && dataJson.map(x => (
-
-                    <div className={`${x.id} grid-item`} key={uuidv4()}>
-                        <img src={`./images/icon-${x.id}.svg`} alt="icon" />
-                        <div className="item-container">
-                            <div className="current-time">
-                                <h2>{x.title}</h2>
-                                {timeFrame === "daily" ? <p>{x.timeframes.daily.current}hrs</p> : timeFrame === "weekly" ? <p>{x.timeframes.weekly.current}hrs</p> : <p>{x.timeframes.monthly.current}hrs</p>}
-                            </div>
-                            <div className="previous-time">
-                                <img src="./images/icon-ellipsis.svg" alt="icon ellipsis"/>
-                                {timeFrame === "daily" ? <p>Last day -{x.timeframes.daily.previous}hrs</p> : timeFrame === "weekly" ? <p>Last week -{x.timeframes.weekly.previous}hrs</p> : <p>Last month -{x.timeframes.monthly.previous}hrs</p>}
+            <div className="container">
+                <MainGridContainer setTimeFrame={setTimeFrame} />
+                <div className="grid">
+                    {timeFrame && dataJson.map(x => (
+                        <div className={`${x.id} grid-item`} key={uuidv4()}>
+                            <img src={`./images/icon-${x.id}.svg`} alt="icon" />
+                            <div className="item-container">
+                                <div className="current-time">
+                                    <h2>{x.title}</h2>
+                                    {timeFrame === "daily" ? <p>{x.timeframes.daily.current}hrs</p> : timeFrame === "weekly" ? <p>{x.timeframes.weekly.current}hrs</p> : <p>{x.timeframes.monthly.current}hrs</p>}
+                                </div>
+                                <div className="previous-time">
+                                    <img src="./images/icon-ellipsis.svg" alt="icon ellipsis" />
+                                    {timeFrame === "daily" ? <p>Last day -{x.timeframes.daily.previous}hrs</p> : timeFrame === "weekly" ? <p>Last week -{x.timeframes.weekly.previous}hrs</p> : <p>Last month -{x.timeframes.monthly.previous}hrs</p>}
+                                </div>
                             </div>
                         </div>
-                    </div>
-
-                ))}
+                    ))}
+                </div>
             </div>
         </main>
     )
